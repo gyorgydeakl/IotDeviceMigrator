e9ccb500e9d117b14eea8e89661cf5d7ac0f763f
gyorgydeakl <levente.gyorgydeak@credit4sales.hu>
2025-07-14 16:21:27 +0200

    Add Azure IoT Hub device method invocation example

--- Program.cs
+++ Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Microsoft.Azure.Devices;
+
+const string IotHubConnectionString =
+    "HostName=cmgtestIoTHub2.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=+6Og9v6MwNPazLiM7yqchGQOvl5Zs61tFAIoTCGrceU=";
+const string DeviceId   = "device01";
+const string MethodName = "asd";
+
+using var serviceClient =
+    ServiceClient.CreateFromConnectionString(IotHubConnectionString);
+
+var methodInvocation = new CloudToDeviceMethod(MethodName)
+{
+    // How long you’ll wait for the device to respond
+    ResponseTimeout   = TimeSpan.FromSeconds(30),
+    // (Optional) how long to wait for IoT Hub to connect to the device
+    ConnectionTimeout = TimeSpan.FromSeconds(10)
+};
+
+// (Optional) JSON payload for the device method
+methodInvocation.SetPayloadJson("{\"example\":\"data\"}");
+
+var result = await serviceClient.InvokeDeviceMethodAsync(DeviceId, methodInvocation);
+
+Console.WriteLine($"Status  : {result.Status}");
+Console.WriteLine($"Payload : {result.GetPayloadAsJson()}");
